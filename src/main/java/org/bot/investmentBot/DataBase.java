66a4a2b6ef3637fede83e66e6120d54f09b37f94
Danila Anikin <92125582+DanyaLNS@users.noreply.github.com>
2022-09-15 17:45:18 +0300

    Добавление метода обновления properties

--- src/main/java/org/bot/investmentBot/DataBase.java
+++ src/main/java/org/bot/investmentBot/DataBase.java
@@ -9,24 +9,25 @@ import java.util.Properties;
 public class DataBase {
     final static String PATH_TO_RESOURCES = "resources/";
 
-    public static String getPathToPropFile(long id){
+    public static String getPathToPropFile(long id) {
         String fileName = id + ".properties";
         File file = new File(PATH_TO_RESOURCES + fileName);
         try {
             if (file.createNewFile()) {
                 createNewPropFile(file, id);
             }
-        } catch (IOException ex){
+        } catch (IOException ex) {
             System.err.println("IO ex");
         } finally {
             return PATH_TO_RESOURCES + fileName;
         }
     }
-    private static void createNewPropFile(File file, long id) throws IOException{
+
+    private static void createNewPropFile(File file, long id) throws IOException {
         float balance = 0;
         float deposit = 0;
         float savings = 0;
-        Time remainingTime = new Time(0,0,0);
+        Time remainingTime = new Time(0, 0, 0);
         Date profileCreate = new Date();
         boolean isBanned = false;
         long partner = 0;
@@ -38,26 +39,41 @@ public class DataBase {
         writer.flush();
         writer.close();
     }
+
     private static String fillFile(long id, float balance, float deposit,
                                    float savings, Time remainingTime, Date profileCreate,
-                                   boolean isBanned, long partner){
+                                   boolean isBanned, long partner) {
         return String.format("""
-                id = %d
-                balance = %f
-                deposit = %f
-                savings = %f
-                remainingTime = %s
-                profileCreate = %s
-                isBanned = %b
-                partner = %d
-                """,
+                        id = %d
+                        balance = %f
+                        deposit = %f
+                        savings = %f
+                        remainingTime = %s
+                        profileCreate = %s
+                        isBanned = %b
+                        partner = %d
+                        """,
                 id, balance, deposit, savings, remainingTime, profileCreate, isBanned, partner);
     }
-    // TODO Реализовать функции перезаписи полей
-    public static void rewriteBalance(Properties prop, String fileName, int balance){
-        try (FileOutputStream out = new FileOutputStream(fileName);){
-            prop.setProperty("country", "america");
-            prop.store(out, null);
+
+    public static void rewriteVariables(String fileName, long id, float balance, float deposit,
+                                        float savings, Time remainingTime, Date profileCreate,
+                                        boolean isBanned, long partner) {
+        try (FileInputStream in = new FileInputStream(fileName);
+             FileOutputStream out = new FileOutputStream(fileName)) {
+            Properties props = new Properties();
+            props.load(in);
+
+            props.setProperty("id", Long.toString(id));
+            props.setProperty("balance", Float.toString(balance));
+            props.setProperty("deposit", Float.toString(deposit));
+            props.setProperty("savings", Float.toString(savings));
+            props.setProperty("remainingTime", remainingTime.toString());
+            props.setProperty("profileCreate", profileCreate.toString());
+            props.setProperty("isBanned", Boolean.toString(isBanned));
+            props.setProperty("partner", Long.toString(partner));
+
+            props.store(out, null);
         } catch (FileNotFoundException ex) {
             System.err.println("File not found");
             ex.printStackTrace();
