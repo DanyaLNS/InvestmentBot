af3803933bcd10fb59aeb763dbb295f87211598f
Danila Anikin <92125582+DanyaLNS@users.noreply.github.com>
2022-09-15 11:41:33 +0300

    Добавление новых сообщений

--- src/main/java/org/bot/investmentBot/MessageCreator.java
+++ src/main/java/org/bot/investmentBot/MessageCreator.java
@@ -18,17 +18,67 @@ public class MessageCreator {
                 "\uD83E\uDDED Время до сбора средств: " + remainingTime;
     }
 
-    public static String getWalletText(Update update, Time remainingTime, float balance, int partners) {
+    public static String getWalletText(Update update, Time remainingTime, float balance, long partner) {
         return String.format(
                 "\uD83E\uDD16 Ваш ID: %d\n" +
-                        "\uD83D\uDCC6 Профиль создан: %s\n" +
+                        "\uD83D\uDCC6 Профиль создан: %s\n\n" +
                         "\uD83D\uDCB3 Ваш баланс: %.2f\n" +
-                        "\uD83D\uDC65 Партнеров: %d чел.", update.message().from().id(), remainingTime, balance, partners);
+                        "\uD83D\uDC65 Партнеров: %d чел.", update.message().from().id(), remainingTime, balance, partner);
     }
-    public static String getInvestCallback(){
+
+    public static String getPartnersText(float deposit, long partners, String referalLink, String outerReferalLink) {
+        return
+                "▪️Партнерская программа создана для того чтобы получить клиенту дополнительный источник дохода, приглашайте людей по своей ссылке\n" +
+                        "\n" +
+                        "\uD83D\uDCB8 Процент с инвестиций: 25.0% \n" +
+                        "\uD83D\uDCB0 Процент с вывода: 15.0% \n" +
+                        "\n" +
+                        "\uD83D\uDCB3 Всего заработано: " + deposit + "Р \n" +
+                        "\n" +
+                        "\uD83D\uDC65 Партнеров: " + partners + "\n" +
+                        "\n" +
+                        "\uD83D\uDD17 Ваша реф-ссылка: \n" + referalLink +
+                        "\n" +
+                        "〽️ Внешняя реф-ссылка: " + outerReferalLink;
+    }
+
+    public static String getSettingsText(int daysOfWork, int amountOfInvestors,
+                                         int newInvestors, int onlineInvestors) {
+        return String.format("▪️ Вы попали в раздел настройки бота, здесь вы можете посмотреть статистику, а также узнать нужную информацию или отключить новые уведомления в нашем боте \n" +
+                "\n" +
+                "\uD83C\uDF10 Дней работаем: %d\n" +
+                "▪️ Всего инвесторов: %d\n" +
+                "▪️ Новых за 24 часа: %d\n" +
+                "▪️ Онлайн: %d", daysOfWork, amountOfInvestors, newInvestors, onlineInvestors);
+    }
+
+    public static String getCaclulatorText(float sum, float percent) {
+        double day = compoundInterest(sum, percent, 1);
+        double mounth = compoundInterest(sum, percent, 30);
+        double year = compoundInterest(sum, percent, 360);
+
+        return String.format("\uD83D\uDCB1 В данном разделе Вы сумеете рассчитать Вашу прибыль, от суммы вашей инвестиции в наш проект:\n" +
+                "\n" +
+                "\uD83D\uDCB5 Ваша инвестиция: %f₽\n" +
+                "\n" +
+                "▪️ Прибыль в сутки: %f₽\n" +
+                "▪️ Прибыль в месяц: %f₽\n" +
+                "▪️ Прибыль в год: %f₽", sum, day, mounth, year);
+    }
+
+    private static double compoundInterest(float sum, float percent, int period) {
+        return Math.round(sum * Math.pow((1 + 0.01 * percent), period));
+    }
+
+    public static String getInvestCallback() {
         return "\uD83D\uDEABПополните баланс, минимальная сумма для инвестиции: 100.0₽";
     }
-    public static String getFlushCallback(){
+
+    public static String getFlushCallback() {
         return "\uD83D\uDEABМинимальная сумма сбора: 1.0₽";
+    }
+
+    public static String getSoonCallback() {
+        return "Этот раздел будет доступен позже";
     }
 }
